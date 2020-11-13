4fa4c13a83063fb6a349e9d8417047137fb2b8d8
rohanphillips <rohan@solutions-insurance.com>
2020-11-13 13:08:15 -0600

    commit tree e28315d6f81de75f338c4f988a74c41fb09ea657

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  bombDecision = () => {
+    let myReturn;
+    if (this.state.secondsLeft > 0){
+      return `${this.state.secondsLeft} seconds left before I go boom!`
+    } else {
+      return "Boom!"
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.bombDecision()}
+      </div>
+    );
+  }
+}
